09407213404b77b73ebf87cae26ccfff89fcc655
StrangerandDanger <11gamerking12@gmail.com>
2024-09-17 11:40:26 +0200

    interface PrimeSieve added and finished

---
+++ GitTestProject/src/PrimeSieve.java
@@ -0,0 +1,7 @@
+public interface PrimeSieve
+{
+    //Ueberprueft ob p eine Primzahl ist
+    public boolean isPrime(int p);
+    //Gibt alle Primzahlen bis zur Obergrenze auf der Konsole aus
+    public void printPrimes();
+}
