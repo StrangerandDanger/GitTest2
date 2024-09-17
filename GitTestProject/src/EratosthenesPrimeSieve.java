324fe2d58dc69ddd81e0558472bf245a6f492139
StrangerandDanger <11gamerking12@gmail.com>
2024-09-17 11:51:51 +0200

    class EratosthenesPrimeSieve added, constructor generated, method PrimeSieve added

---
+++ GitTestProject/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,15 @@
+
+public class EratosthenesPrimeSieve
+{
+  int MAX;
+
+  public EratosthenesPrimeSieve(int MAX)
+  {
+    this.MAX = MAX;
+  }
+
+  public void PrimeSieve(String[] args)
+  {
+    
+  }
+}
